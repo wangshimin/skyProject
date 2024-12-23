b388fcf73c0edae0b01127db3e44ec4759f0523e
wangshimin <shimin.wang@foxmail.com>
2024-12-23 19:07:57 +0800

    员工 - 分页查询

--- server/sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ server/sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.sky.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,34 +14,46 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "员工信息")
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("密码")
     private String password;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
+    @ApiModelProperty("状态 0:禁用，1:正常")
     private Integer status;
 
+    @ApiModelProperty("创建时间")
 //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
+    @ApiModelProperty("更新时间")
 //    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
+    @ApiModelProperty("创建人")
     private Long createUser;
 
+    @ApiModelProperty("修改人")
     private Long updateUser;
 
 }
