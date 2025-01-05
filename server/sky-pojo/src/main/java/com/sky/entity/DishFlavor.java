f631ff4e0a16146ac8fca8ced0e09d6eb6a63785
wangshimin <shimin.wang@foxmail.com>
2025-01-05 17:43:32 +0800

    菜品模块 - 新增菜品

--- server/sky-pojo/src/main/java/com/sky/entity/DishFlavor.java
+++ server/sky-pojo/src/main/java/com/sky/entity/DishFlavor.java
@@ -1,5 +1,7 @@
 package com.sky.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +16,22 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "菜品口味")
 public class DishFlavor implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty(value = "主键")
     private Long id;
-    //菜品id
+
+    @ApiModelProperty(value = "菜品id")
     private Long dishId;
 
-    //口味名称
+    @ApiModelProperty(value = "口味名称", required = true)
     private String name;
 
     //口味数据list
+    @ApiModelProperty(value = "口味值", required = true)
     private String value;
 
 }
