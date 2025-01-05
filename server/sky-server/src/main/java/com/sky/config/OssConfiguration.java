28e27ffeb25dba06443dadc51100c252edd02e9d
wangshimin <shimin.wang@foxmail.com>
2025-01-05 15:52:54 +0800

    菜品模块 - 文件上传

---
+++ server/sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,27 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil 对象
+ */
+@Configuration // 配置类
+@Slf4j
+public class OssConfiguration {
+
+    @Bean   // 将AliOssUtil对象存入容器中
+    @ConditionalOnMissingBean // 当容器中没有AliOssUtil对象时，创建该对象
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象：{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+
+    }
+}
