10d337434a5ece84b2c64dedf96ee507e081fe60
wangshimin <shimin.wang@foxmail.com>
2025-01-06 15:47:21 +0800

    菜品模块 - 删除菜品

--- server/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ server/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -6,8 +6,11 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -32,4 +35,25 @@ public interface DishMapper {
      * @return
      */
     Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据主键id查询菜品数据
+     * @param id
+     * @return
+     */
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+
+    /**
+     * 根据主键id删除菜品数据
+     * @param id
+     */
+    @Delete("delete from dish where id = #{id}")
+    void deleteById(Long id);
+
+    /**
+     * 根据菜品id集合批量删除菜品数据
+     * @param ids
+     */
+    void deleteByIds(List<Long> ids);
 }
--- server/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ server/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -2,12 +2,16 @@ package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
 import com.sky.result.PageResult;
 import com.sky.service.DishService;
 import com.sky.vo.DishVO;
@@ -27,6 +31,8 @@ public class DishServiceImpl implements DishService {
     private DishMapper dishMapper;
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
 
     /**
      * 新增菜品和对应的口味
@@ -69,5 +75,43 @@ public class DishServiceImpl implements DishService {
         List<DishVO> records = page.getResult(); // 当前页数据
         // 返回结果
         return new PageResult(total, records);
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    @Override
+    @Transactional // 开启事务
+    public void deleteBatch(List<Long> ids) {
+
+        for (Long id : ids) {
+            Dish dish = dishMapper.getById(id);
+            // 判断当前菜品状态
+            if (dish.getStatus() == StatusConstant.ENABLE) {
+                // 当前菜品处于起售中，不能删除
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+
+        // 判断当前菜品是否被套餐关联
+        List<Long> setmealIds = setmealDishMapper.getSetmealIdByDishIds(ids);
+        if (setmealIds != null && setmealIds.size() > 0) {
+            // 当前菜品被套餐关联，不能删除
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+
+        // 删除菜品表中的菜品数据
+        /*for (Long id : ids) {
+            dishMapper.deleteById(id);
+            // 删除菜品口味表中的数据
+            dishFlavorMapper.deleteByDishId(id);
+        }*/
+
+        // 根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+
+        // 根据菜品id集合批量删除关联的口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 }
