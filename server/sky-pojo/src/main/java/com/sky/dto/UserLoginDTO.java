d5787c043a22e0aa4bbf666aeb9430122f202645
wangshimin <shimin.wang@foxmail.com>
2025-01-16 17:11:38 +0800

    微信登录

--- server/sky-pojo/src/main/java/com/sky/dto/UserLoginDTO.java
+++ server/sky-pojo/src/main/java/com/sky/dto/UserLoginDTO.java
@@ -1,5 +1,7 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,8 +10,10 @@ import java.io.Serializable;
  * C端用户登录
  */
 @Data
+@ApiModel(description = "用户登录")
 public class UserLoginDTO implements Serializable {
 
+    @ApiModelProperty("微信授权码")
     private String code;
 
 }
