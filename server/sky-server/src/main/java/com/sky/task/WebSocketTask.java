0f02fc8e2a2cfc70321496ef11384c178881bcdc
wangshimin <shimin.wang@foxmail.com>
2025-03-20 14:54:35 +0800

    WebSocket入门 实现步骤： 1.准备一个html页面作为客户端 2.定义WebSocket的maven坐标 3.定义服务端组件WebSocketServer，用于和客户端通信 4.定义配置类WebSocketConfiguration，注册WebSocket的服务端组件

---
+++ server/sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -0,0 +1,24 @@
+package com.sky.task;
+
+import com.sky.websocket.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class WebSocketTask {
+
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    /**
+     * 定时任务，通过webSocket每隔5秒发送一条消息到客户端
+     */
+    @Scheduled(cron = "0/5 * * * * ?") // 每5秒执行一次
+    public void sendMessageToClient() {
+        webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
+    }
+}
